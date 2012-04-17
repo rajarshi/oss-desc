92a9c2b4513408de6608c2c21f5f8f35d10a8424
Rajarshi Guha <rajarshi.guha@gmail.com>
2012-04-17 09:17:48 -0400

    Updated some figure

---
+++ code/code.R
@@ -0,0 +1,39 @@
+library(lattice)
+library(reshape)
+
+acd <- read.table('all-clean.acd', sep='\t', header=TRUE, as.is=TRUE)
+cdk <- read.table('all-clean.cdk', sep='\t', header=TRUE, as.is=TRUE)
+
+tmp <- data.frame(ACD=acd$ACD_RuleOf5_PSA, CDK=cdk$TopoPSA)
+xyplot(ACD ~ CDK, tmp, type=c('p', 'g'), col='black', cex=0.25,
+       xlab=list(label='CDK TPSA', cex=1.2),
+       ylab=list(label='ACD TPSA', cex=1.2),
+       panel=function(x,y,...) {
+         panel.xyplot(x,y,...)
+         panel.abline(0, 1, col='red')                  
+       }
+         )
+dev.copy(pdf, '../tpsa-cdk-acd.pdf', 6,6);dev.off()
+dev.copy(png, '../tpsa-cdk-acd.png', 400,400);dev.off()
+
+
+## volumes
+moe <- read.csv('vol.moe', header=TRUE, as.is=TRUE)
+cdk <- read.table('vol.cdk', header=TRUE, as.is=TRUE, sep=' ')
+names(moe) <- c('sid', 'vdw_vol', 'vol')
+names(cdk) <- c('sid', 'VABC')
+tmp <- merge(moe, cdk, by='sid')
+tmp <- tmp[ complete.cases(tmp), ]
+tmp <- melt(tmp, measure.vars=c('vdw_vol', 'vol'), id.vars='VABC')
+xyplot(value ~ VABC | variable, tmp,
+       type=c('p', 'g'), col='black', cex=0.25,
+       xlab=list(label='CDK VABC', cex=1.2),
+       ylab=list(label='MOE', cex=1.2),
+       xlim=range(c(tmp$VABC, tmp$value)),
+       ylim=range(c(tmp$VABC, tmp$value)),       
+        panel=function(x,y,...) {
+         panel.xyplot(x,y,...)
+         panel.abline(0, 1, col='red')                  
+       })
+dev.copy(pdf, '../vol-moe-cdk.pdf', 8,4);dev.off()
+dev.copy(png, '../vol-moe-cdk.png', 600,300);dev.off()
