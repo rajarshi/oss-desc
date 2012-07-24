1af6ba5142ede015550fc3e8b93a6415544ed7b2
Rajarshi Guha <rajarshi.guha@gmail.com>
2012-07-24 16:21:45 -0400

    updated plots

--- code/code.R
+++ code/code.R
@@ -1,5 +1,6 @@
 library(lattice)
 library(reshape)
+library(ggplot2)
 
 acd <- read.table('all-clean.acd', sep='\t', header=TRUE, as.is=TRUE)
 cdk <- read.table('all-clean.cdk', sep='\t', header=TRUE, as.is=TRUE)
@@ -20,11 +21,19 @@ dev.copy(png, '../tpsa-cdk-acd.png', 400,400);dev.off()
 ## volumes
 moe <- read.csv('vol.moe', header=TRUE, as.is=TRUE)
 cdk <- read.table('vol.cdk', header=TRUE, as.is=TRUE, sep=' ')
-names(moe) <- c('sid', 'vdw_vol', 'vol')
+names(moe) <- c('sid', 'van der Waals Volume (2D)', 'van der Waals Volume (grid based)')
 names(cdk) <- c('sid', 'VABC')
 tmp <- merge(moe, cdk, by='sid')
 tmp <- tmp[ complete.cases(tmp), ]
-tmp <- melt(tmp, measure.vars=c('vdw_vol', 'vol'), id.vars='VABC')
+tmp <- melt(tmp,, id.vars='VABC', measure.vars=c('van der Waals Volume (2D)', 'van der Waals Volume (grid based)'))
+
+ggplot(tmp, aes(x=VABC,y=value))+
+  geom_point(size=1,alpha=0.25)+
+  geom_smooth()+
+  facet_wrap(~variable)+
+  scale_x_continuous(limits=c(0,1500))+
+  xlab('CDK VABC')+ylab("MOE")
+
 xyplot(value ~ VABC | variable, tmp,
        type=c('p', 'g'), col='black', cex=0.25,
        xlab=list(label='CDK VABC', cex=1.2),
